4094ab26dc8a327b13863ded622b62db6fe9247d
Pavel R <334554.rud@gmail.com>
2025-12-15 01:26:27 +0300

    Created 1 model, infrastructure for launch DB, role-enums on future

---
+++ app/infrastructure/database/models/tg_users.py
@@ -0,0 +1,22 @@
+import asyncio
+from datetime import datetime
+
+from sqlalchemy import BigInteger, Text, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.infrastructure.database import Base
+
+
+class TgUser(Base):
+    __tablename__ = "tg_users"
+
+    tg_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    first_name: Mapped[str] = mapped_column(Text, nullable=False)
+    phone_number: Mapped[str] = mapped_column(Text, nullable=False)
+    role: Mapped[str] = mapped_column(Text, nullable=False)
+    language: Mapped[str] = mapped_column(Text, nullable=False)
+    banned: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    is_alive: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
