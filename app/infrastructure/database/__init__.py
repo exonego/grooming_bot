8cb376acb933cf6e88399bec6c9e328156e985a1
exonego <334554.rud@gmail.com>
2025-12-18 23:17:34 +0300

    Delete app directory

--- app/infrastructure/database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import Base
-from .models import TgUser, PhoneClient, Weekday, RecordDate, Record
-
-__all__ = ["Base", "TgUser", "PhoneClient", "Weekday", "RecordDate", "Record"]
--- app/infrastructure/database/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .tg_users import TgUser
-from .phone_clients import PhoneClient
-from .weekdays import Weekday
-from .record_dates import RecordDate
-from .records import Record
-
-__all__ = ["TgUser", "PhoneClient", "Weekday", "RecordDate", "Record"]
