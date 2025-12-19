b7c7a41656594129032280e0328475f6f979ac0d
Pavel R <334554.rud@gmail.com>
2025-12-19 19:04:07 +0300

    i18n middleware added

--- bot/middlewares/i18n.py
+++ bot/middlewares/i18n.py
@@ -6,7 +6,10 @@ from aiogram import BaseMiddleware
 from aiogram.types import Update, User
 from fluentogram import TranslatorHub
 
+from infrastructure.database import TgUser
+
 logger = logging.getLogger(__name__)
+
 
 class TranslatorRunnerMiddleware(BaseMiddleware):
     async def __call__(
@@ -15,4 +18,19 @@ class TranslatorRunnerMiddleware(BaseMiddleware):
         event: Update,
         data: dict[str, Any],
     ) -> Any:
-        pass
+
+        user: User = data.get("event_from_user")
+
+        if user is None:
+            return await handler(event, data)
+
+        db_user: TgUser = data.get("db_user")
+        if db_user:
+            user_lang = db_user.language
+        else:
+            user_lang = user.language_code
+
+        hub: TranslatorHub = data.get("_translator_hub")
+        data["i18n"] = hub.get_translator_by_locale(locale=user_lang)
+
+        return await handler(event, data)
